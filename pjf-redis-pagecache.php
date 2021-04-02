73c34e7476f6d1a83480b6208ff9e02b537719c7
Yui ★ღツ ゆい <43382763+fierevere@users.noreply.github.com>
2021-04-02 18:52:13 +0300

    add plugin mainfile

---
+++ pjf-redis-pagecache.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: PJF Redis PageCache
+ * Plugin URI: https://github.com/fierevere/pjf-redis-pagecache
+ * Description: Simple WordPress pagecache for redis backend
+ * Version: 0.9
+ * License: GPLv3 or later
+ *
+ * To install create a symbolic link to the advanced-cache.php dropin from
+ * your wp-content directory and enable page caching in your wp-config.php file.
+ *
+ * Refer to the following URL for further instructions:
+ *
+ * https://github.com/fierevere/pjf-redis-pagecache
+ *
+ * Please keep this helper plugin active for update notifications and future
+ * helper and CLI functionality.
+ */
